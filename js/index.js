583098d9625a561ae1fb6f880ba7fd416bd5635d
Sagar Chand Agarwal <atomsagar@gmail.com>
2015-12-06 15:53:33 +0530

    update

--- js/index.js
+++ js/index.js
@@ -5,9 +5,8 @@ $(function() {
     // Replace this line with the one on your Quickstart Guide Page
 	Parse.initialize("ORdRb6ZtoTRSIUBeDKCcpigKQR9Pr6YJjeguuN9t", "ec8NHsZDP17pTdRjBKQFAPxB8ihduGt6e8NAGdaH"); 
 	var TestObject = Parse.Object.extend("TestObject");
-var testObject = new TestObject();
-testObject.save({foo: "bar"}).then(function(object) {
-  alert("yay! it worked");
-});
+	var testObject = new TestObject();
+	testObject.save({foo: "bar"}).then(function(object) {
+	});
 });
 
