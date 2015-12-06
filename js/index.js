764b49dc00bd0a2f31406cdbee484b94b1855172
Sagar Chand Agarwal <atomsagar@gmail.com>
2015-12-06 12:37:45 +0530

    add:

--- js/index.js
+++ js/index.js
@@ -3,30 +3,11 @@ $(function() {
     Parse.$ = jQuery;
  
     // Replace this line with the one on your Quickstart Guide Page
-    Parse.initialize("ORdRb6ZtoTRSIUBeDKCcpigKQR9Pr6YJjeguuN9t", "ec8NHsZDP17pTdRjBKQFAPxB8ihduGt6e8NAGdaH");
- 
+	Parse.initialize("ORdRb6ZtoTRSIUBeDKCcpigKQR9Pr6YJjeguuN9t", "ec8NHsZDP17pTdRjBKQFAPxB8ihduGt6e8NAGdaH"); 
+	var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({foo: "bar"}).then(function(object) {
+  alert("yay! it worked");
+});
 });
 
-$('.form-signin').on('submit', function(e) {
- 
-    // Prevent Default Submit Event
-    e.preventDefault();
- 
-    // Get data from the form and put them into variables
-    var data = $(this).serializeArray(),
-        username = data[0].value,
-        password = data[1].value;
- 
-    // Call Parse Login function with those variables
-    Parse.User.logIn(username, password, {
-        // If the username and password matches
-        success: function(user) {
-            alert('Welcome!');
-        },
-        // If there is an error
-        error: function(user, error) {
-            console.log(error);
-        }
-    });
- 
-});
